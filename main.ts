26b3c655e12776cfe04b2233eca376f572cb4c5d
fu1819 <71689568+fu1819@users.noreply.github.com>
2020-11-03 15:28:20 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
 basic.forever(function () {
-	
+    for (let I = 0; I <= 4; I++) {
+        for (let j = 0; j <= I + 1; j++) {
+            led.plot(4 - I + j, j)
+        }
+        basic.pause(500)
+    }
+    for (let I = 0; I <= 3; I++) {
+        for (let j = 0; j <= 4 - I; j++) {
+            led.plot(j, I + j + 1)
+        }
+        basic.pause(500)
+    }
 })
